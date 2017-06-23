591a26634cd9969e45aaaceee7ccf2d4554a63a0
gao <865636886@qq.com>
2017-06-23 18:28:04 +0800

    init project

---
+++ src/Md5Hasher.php
@@ -0,0 +1,21 @@
+<?php
+namespace GeekGhc\LaraFlash;
+
+class MD5Hasher
+{
+    public function make($value,array $option = [])
+    {
+        $salt = isset($option['salt'])? $option['salt']:'';
+        return hash('md5',$value.$salt);
+    }
+
+    public function check($value,$hashValue,array $option = [])
+    {
+        $salt = isset($option['salt'])? $option['salt']:'';
+
+        return hash('md5',$value.$salt) === $hashValue;
+
+    }
+
+
+}
